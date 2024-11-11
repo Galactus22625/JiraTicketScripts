878ca0f2db349be30db1222146671dd04a773b6b
Maxwell Brown <mxwelwbr@amazon.com>
2024-11-11 12:34:59 -0800

    read me changes and further steps
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Jira Ticket Scripts.
-Generate CSV files with generated Jira Ticket information to import into Jira for volume testing using GenerateCSV.py
+Generate CSV files with generated Jira Ticket information to import into Jira for volume testing using GenerateCSV.py <br />
 Automatically update tickets with UpdateTickets.py using Jira api.
 
 
 ## Use (UpdateTickets.py)
-To run update tickets, add the link to your jira projects rest api url ino the ACCOUNT_URL variable in UpdateTickets.py.  Then run with
+To run update tickets, enter the following command.  
 >python3 UpdateTickets.py --user [jira-username] --password [jira-api-key] --url [url-of-your-jira-site] --projkey [list of project keys]
+
 This will take every ticket in the project and update the description and summary to something new.
+
+To generate an api key for the passwrod field, follow the instructions here [Jira API KEY instuctions.](https://support.atlassian.com/atlassian-account/docs/manage-api-tokens-for-your-atlassian-account/)
 
 
 ## Use (GenerateCSV.py)
@@ -22,3 +25,6 @@ To upload generated tickets to Jira, import the generated CSV file.
 You may need to use the old Jira UI to upload to multiple projects properly.  Do not map Issue ID for the old Jira UI.   You may also run into Issues mapping Statuses into projects you already have in Jira if the mapping does not exist.  To avoid you can change the statuses fields to ones that match, or just dont map status.
 
 If you want to upload a business project instead of a software project, change the project type category to business.  You may also need to alter the possible statuses.
+
+## Further Updates Anyone can add
+Create a createTickets.py to create tickets using the same api as updateTickets.py.  Can be used instead of GenerateCSV.py
--- UpdateTickets.py
+++ UpdateTickets.py
@@ -8,24 +8,35 @@ API_LINK = "/rest/api/2/"
 def main():
     args = parseArguments()
     auth = (args.user, args.password)
-    editIssue("ED-1", args.url, auth)
+    print(args.projkey)
+    for project in args.projkey:
+        updateProjectIssues(project)
+    editIssue("ED-1", args.url, auth, "this is the new summary", "this is the new description")
+
+def updateProjectIssues(projectKey):
     pass
 
-def editIssue(issueKey, url, auth):
-apiUrl = url + API_LINK + "issue/" + issueKey
+def editIssue(issueKey, url, auth, newSummary, newDescription):
+    apiUrl = url + API_LINK + "issue/" + issueKey
 
-updateJson = {
-    "fields": {"summary": "What fun"}
-}
-response = requests.put(apiUrl, json = updateJson, auth = auth)
-assert (response.status_code == 200 or response.status_code == 204), f"Expected a response code of 200 or 204.  Received a response code of {response.status_code}"
+    updateJson = {
+        "fields": {
+            "summary": newSummary,
+            "description": newDescription
+            }
+    }
+    response = requests.put(apiUrl, json = updateJson, auth = auth)
+    if (response.status_code == 200 or response.status_code == 204):
+        print(f"edited issue {issueKey}")
+    else:
+        print(f"Expected a response code of 200 or 204.  Received a response code of {response.status_code}")
 
 def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', type = str, help = "url of your jira site", required = True)
     parser.add_argument('--user', type = str, help = "your jira username", required = True)
     parser.add_argument('--password', type = str, help = "your jira api key", required = True)
-    parser.add_argument('--projkey', type = str, nargs='+', help = "project keys that you want updated", required = False)
+    parser.add_argument('--projkey', type = str, nargs='+', help = "project keys that you want updated", required = True)
     args = parser.parse_args()
     return args
 
