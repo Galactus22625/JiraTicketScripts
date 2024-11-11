cb22c73a3d0c9626ba0e1e798df341d53244d2f1
Maxwell Brown <mxwelwbr@amazon.com>
2024-11-11 15:52:44 -0800

    now edits all pages
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>

--- UpdateTickets.py
+++ UpdateTickets.py
@@ -10,35 +10,51 @@ API_LINK = "/rest/api/2/"
 
 logging.basicConfig(filename='UpdateTickets.log',level=logging.INFO)
 
+
 def main():
     args = parseArguments()
     auth = (args.user, args.password)
     for project in args.projkey:
         updateProjectIssues(project, args.url, auth)
 
+
 def updateProjectIssues(projectKey, url, auth):
-    apiUrl = url + API_LINK + "search?jql= project in (\"" + projectKey +"\")"
-    response = requests.get(apiUrl, auth = auth)
-    if response.status_code != 200:
-        logging.error(f"Expected a response code of 200.  Received a response code of {response.status_code}")
-        print(f"Unable to access project: {projectKey}, please see logs")
+    result = getPageData(projectKey, url, auth)
+    if result == None:
         return
-
-    data = response.json()
-    issues = data["issues"]
-    letters = string.ascii_lowercase + " "
+    data, totalIssues, maxResults = result
+    startAt = 0
     ticketsEdited = 0
-    for issue in issues:
-        key = issue["key"]
-        description = issue["fields"]["description"]
-        summary = issue["fields"]["summary"]
 
-        newDescription = ''.join(random.choice(letters) for i in range(random.randint(5, 2000))) + "\n The Description has been Replaced."
-        newSummary = summary + ". We now have a new summary."
-        ticketsEdited += editIssue(key, url, auth, newSummary, newDescription)
+    while startAt < totalIssues:
+        apiUrl = url + API_LINK + "search?jql= project in (\"" + projectKey +"\")" + "&startAt=" + str(startAt)
+        startAt += maxResults
+        response = requests.get(apiUrl, auth = auth)
+        if response.status_code != 200:
+            logging.error(f"Expected a response code of 200.  Received a response code of {response.status_code} for page number {startAt/maxResults}")
+            print(f"Unable to access project: {projectKey} page number {startAt/maxResults}, please see logs")
+            return
 
-    print(f"Succesfully edited {ticketsEdited} tickets in project {projectKey}")
+        data = response.json()
+        issues = data["issues"]
+        letters = string.ascii_lowercase + " "
+        batch = 0
+        for issue in issues:
+            key = issue["key"]
+            description = issue["fields"]["description"]
+            summary = issue["fields"]["summary"]
+
+            newDescription = ''.join(random.choice(letters) for i in range(random.randint(5, 2000))) + "\n The Description has been Replaced."
+            newSummary = "This is the new summary of issue " + key
+            result = editIssue(key, url, auth, newSummary, newDescription)
+            if result > 0:
+                batch+= result
+                ticketsEdited += result
+                print(f"Succesfully edited {ticketsEdited} tickets in project {projectKey}", end='\r')
+        logging.debug(f"Processed a batch of {batch} tickets.")
+    print(f"Succesfully edited a total of {ticketsEdited} tickets in project {projectKey}")
     logging.info(f"Succesfully edited {ticketsEdited} tickets in project {projectKey}")
+
 
 def editIssue(issueKey, url, auth, newSummary, newDescription):
     apiUrl = url + API_LINK + "issue/" + issueKey
@@ -58,6 +74,20 @@ def editIssue(issueKey, url, auth, newSummary, newDescription):
         print("Error editing an issue, see logs")
         return 0
 
+
+def getPageData(projectKey, url, auth):
+    apiUrl = url + API_LINK + "search?jql= project in (\"" + projectKey +"\")"
+    response = requests.get(apiUrl, auth = auth)
+    if response.status_code != 200:
+        logging.error(f"Expected a response code of 200.  Received a response code of {response.status_code}")
+        print(f"Unable to access project: {projectKey}, please see logs")
+        return None
+    data = response.json()
+    totalIssues = data["total"]
+    maxResults = data["maxResults"]
+    return data, totalIssues, maxResults
+
+
 def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--url', type = str, help = "url of your jira site", required = True)
@@ -66,6 +96,7 @@ def parseArguments():
     parser.add_argument('--projkey', type = str, nargs='+', help = "project keys that you want updated", required = True)
     args = parser.parse_args()
     return args
+
 
 if __name__ == "__main__":
     main()
